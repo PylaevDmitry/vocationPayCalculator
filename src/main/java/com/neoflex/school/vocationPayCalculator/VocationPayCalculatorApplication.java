3a92add55dbe0fd77dd3259dfa95c758419e2833
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-04-26 11:33:23 +0400

    initial

---
+++ src/main/java/com/neoflex/school/vocationPayCalculator/VocationPayCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.school.vocationPayCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocationPayCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocationPayCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoflex/school/vocationPayCalculator/VocationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoflex.school.vocationPayCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
